c5e373786ed6c661b8bba52d9dd50afa772b0db6
Chris Green <w0795059@myscc.ca>
2023-01-20 22:04:58 -0500

    Things added to ContentList Class * Private array of type string * Constructor that sets the array to empty * A getter method * Add content to the array method * ReturnLength of array method

--- C_Green_Streetwear/src/app/models/content-list.ts
+++ C_Green_Streetwear/src/app/models/content-list.ts
@@ -1,2 +1,24 @@
+import { IContent } from "./icontent";
+
 export class ContentList {
+
+    private _contentArray: IContent[];
+
+    constructor(_array: IContent){
+        this._contentArray = [];
+    }
+
+    get array(): IContent[]{
+        return this._contentArray;
+    }
+
+    addContent(content: IContent){
+        this._contentArray.push(content);
+    }
+
+    returnLength(): number {
+        return this._contentArray.length;
+    }
+
+
 }
