9c1f9abe3783f9369cc00bf00786b268cfa3c847
Chris Green <w0795059@myscc.ca>
2023-02-05 23:07:22 -0500

    Steps 10-13 added but not fully working

--- C_Green_Streetwear/src/app/app.component.ts
+++ C_Green_Streetwear/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 // import { ContentList } from './models/content-list';
 import { IContent } from './models/icontent';
+import { StreetwearService } from './services/streetwear.service';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +11,17 @@ import { IContent } from './models/icontent';
 export class AppComponent {
   title = 'C_Green_Streetwear';
 
-  constructor(){
+  content: IContent[] = [];
+  selectedContent: IContent;
 
-  let contentCoat: IContent = {id: 1,title: "Supreme x North Face Jacket", author: "Supreme x Northface", description: "This supreme coat is the supreme red with the brand name on the back.", 
-  imgSrc: "assets/images/snj.jpg", type: "Winter Collection 2015"};
-
-  let contentBag: IContent = {id: 1,title: "Supreme x North Face Bag", author: "Supreme", description: "A collaboration between Supreme and North Face. This bag was a once only drop. ", 
-  imgSrc: "assets/images/bag.jpg", type: "Summer Collection 2011"};
-
-  let contentHat: IContent = {id: 1,title: "Supreme x Lacoste Hat", author: "Supreme", description: "A collaboration between Supreme and Lacoste. These two brands come together to bring a new spin on the 'dad' hat.", 
-  imgSrc: "assets/images/hat.jpg", type: "Summer Collection 2012"};
-
-  let contentSkateBoard: IContent = {id: 1,title: "Supreme x Kaws Skate Board", author: "Supreme", description: "Kaws is known for their figurines and crazy style. This skateboard is a collaboration between Supreme and Kaws. ", 
-  imgSrc: "assets/images/skateboard.jpg", type: "Spring Collection 2020"};
-
+  constructor(private streetwearService: StreetwearService){
+    this.selectedContent = this.content[0];
   }
+
+  ngOnInit(): void {
+    this.streetwearService.getContentById(1).subscribe((content: IContent[]) =>{
+      this.content = content;
+    });
+  }
+
 }
--- C_Green_Streetwear/src/app/services/streetwear.service.ts
+++ C_Green_Streetwear/src/app/services/streetwear.service.ts
@@ -7,10 +7,53 @@ import { IContent } from '../models/icontent';
   providedIn: 'root'
 })
 export class StreetwearService {
+  
 
   constructor() { }
 
   getContent(): Observable<IContent[]>{
     return of(CONTENT);
   }
+
+  //Get item bt ID in array
+  getContentById(id: number): Observable<IContent[]> {
+    return of(CONTENT.filter(content => content.id === id));
+  }
+
+  //Add item to the array
+  addContent(newContent: IContent): Observable<IContent[]>{
+    const updatedContent = [...CONTENT, newContent];
+    return of(updatedContent);
+  }
+
+  //Update an item in the array
+  updateContent(updatedContent: IContent): Observable<IContent[]> {
+    let updated = false;
+    const updatedArray = CONTENT.map(content => {
+      if (content.id === updatedContent.id) {
+        updated = true;
+        return updatedContent;
+      }
+      return content;
+    });
+
+    if (!updated) {
+      return of([]);
+    }
+
+    return of(updatedArray);
+  }
+
+  //Remove an item in the array
+  removeContent(id: number): Observable<IContent | undefined> {
+    const content = CONTENT.find(content => content.id === id);
+    if (content) {
+        const index = CONTENT.indexOf(content);
+        CONTENT.splice(index, 1);
+        return of(content);
+    } else {
+        return of(undefined);
+    }
+}
+
 }
