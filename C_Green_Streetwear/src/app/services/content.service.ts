792e05569f6cba4b98aba3c8387d6adc0f9b535b
Chris Green <w0795059@myscc.ca>
2023-04-01 20:51:15 -0400

    Updated ContentService to use the server to perform CRUD

--- C_Green_Streetwear/src/app/services/content.service.ts
+++ C_Green_Streetwear/src/app/services/content.service.ts
@@ -14,6 +14,29 @@ export class ContentService {
 
   constructor(private http: HttpClient) { }
 
+  getContentList(): Observable<IContent[]> {
+    return this.http.get<IContent[]>(this.contentUrl);
+  }
+
+  getContentById(id: number): Observable<IContent> {
+    const url = `${this.contentUrl}/${id}`;
+    return this.http.get<IContent>(url);
+  }
+
+  addContent(content: IContent): Observable<IContent> {
+    return this.http.post<IContent>(this.contentUrl, content);
+  }
+
+  updateContent(content: IContent): Observable<IContent> {
+    const url = `${this.contentUrl}/${content.id}`;
+    return this.http.put<IContent>(url, content);
+  }
+
+  deleteContent(id: number): Observable<IContent> {
+    const url = `${this.contentUrl}/${id}`;
+    return this.http.delete<IContent>(url);
+  }
+
   getContent(): Observable<IContent[]>{
     return this.http.get<IContent[]>(this.contentUrl);
   }
