b274e1eb8ca9f1d1a359f4e391d2d29b03ebc5bd
Chris Green <w0795059@myscc.ca>
2023-04-15 22:38:30 -0400

    Added 3 more items to IContent array

--- C_Green_Streetwear/src/app/data/mock-content.ts
+++ C_Green_Streetwear/src/app/data/mock-content.ts
@@ -7,25 +7,52 @@ export const CONTENT = [{
     description: "This supreme coat is the supreme red with the brand name on the back.", 
     imgSrc: "assets/images/snj.jpg", 
     type: "Winter Collection 2015"
-}, {
+}, 
+{
     id: 2,
     title: "Supreme x North Face Bag", 
     author: "Supreme", 
     description: "A collaboration between Supreme and North Face. This bag was a once only drop. ", 
     imgSrc: "assets/images/bag.jpg", 
     type: "Summer Collection 2011"
-}, {
+}, 
+{
     id: 3,
     title: "Supreme x Lacoste Hat", 
     author: "Supreme", 
     description: "A collaboration between Supreme and Lacoste. These two brands come together to bring a new spin on the 'dad' hat.", 
     imgSrc: "assets/images/hat.jpg", 
     type: "Summer Collection 2012"
-}, {
+}, 
+{
     id: 4,
     title: "Supreme x Kaws Skate Board", 
     author: "Supreme", 
     description: "Kaws is known for their figurines and crazy style. This skateboard is a collaboration between Supreme and Kaws. ", 
     imgSrc: "assets/images/skateboard.jpg", 
     type: "Spring Collection 2020"
+},
+{
+    id: 5,
+    title: "Supreme x Kaws Shirt", 
+    author: "Supreme", 
+    description: "Kaws is known for their figurines and crazy style. This shirt is a collaboration between Supreme and Kaws. ", 
+    imgSrc: "assets/images/kawssweater.jpg", 
+    type: ""  
+},
+{
+    id: 6,
+    title: "Supreme x Thrasher Shirt", 
+    author: "Supreme", 
+    description: "Thrasher is wel known in the skateboarding/clothing community. This shirt is a collaboration between Supreme and Thrasher. ", 
+    imgSrc: "assets/images/thrasher.jpg", 
+    type: "Spring Collection 2020"
+},
+{
+    id: 7,
+    title: "Supreme x Travis Scott Shirt", 
+    author: "Supreme", 
+    description: "Travis Scott is a well known rapper. This shirt is a collaboration between Supreme and Travis Scott. ", 
+    imgSrc: "assets/images/travis.jpg", 
+    type: ""
 }];
